efec677d824b2f40462d2c6909d3cbf6446ed834
Sardo <15870134+oneslash@users.noreply.github.com>
2023-05-20 20:04:38 +0200

    Feat/put object (#9)
    * close #8
    
    * update docs

---
+++ src/apis/put_handler.rs
@@ -0,0 +1,42 @@
+use actix_router::{Path, Resource, ResourceDef, Url};
+
+use crate::apis::create_bucket::create_bucket;
+use crate::apis::put_object::put_object;
+use actix_web::{put, web, Error, HttpRequest, HttpResponse};
+
+#[put("/{tail}*")]
+pub async fn handle_put(
+    _: web::Path<String>,
+    req: HttpRequest,
+    data: web::Data<crate::AppState>,
+    mut payload: Option<web::Payload>,
+) -> Result<HttpResponse, Error> {
+    let mut path = Path::new(Url::new(req.uri().clone()));
+
+    let routes = [
+        ("/{bucket}/{object_name}", "bucket and object"),
+        ("/{bucket}", "bucket"),
+    ];
+
+    for (pattern, description) in &routes {
+        if ResourceDef::new(*pattern).capture_match_info(&mut path) {
+            return match (path.get("bucket"), path.get("object_name")) {
+                (Some(bucket), Some(object_name)) if *description == "bucket and object" => {
+                    put_object(
+                        bucket.to_string(),
+                        object_name.to_string(),
+                        data,
+                        payload.unwrap(),
+                    )
+                    .await
+                }
+                (Some(bucket), None) if *description == "bucket" => {
+                    create_bucket(bucket.to_string(), data).await
+                }
+                _ => return Ok(HttpResponse::InternalServerError().finish()),
+            };
+        }
+    }
+
+    Err(actix_web::error::ErrorNotFound("Not Found"))
+}
---
+++ src/apis/put_object.rs
@@ -0,0 +1,23 @@
+use actix_web::{put, web, Error, FromRequest, HttpRequest, HttpResponse};
+use futures::StreamExt;
+use std::fs::File;
+use std::io::Write;
+
+pub async fn put_object(
+    bucket_name: String,
+    object_name: String,
+    data: web::Data<crate::AppState>,
+    mut payload: web::Payload,
+) -> Result<HttpResponse, Error> {
+    let working_folder = &data.working_folder;
+
+    let filepath = format!("{}/{}/{}", working_folder, bucket_name, object_name);
+    let mut file = File::create(&filepath)?;
+
+    while let Some(bytes) = payload.next().await {
+        let data = bytes.unwrap();
+        file.write_all(&data)?;
+    }
+
+    Ok(HttpResponse::Ok().finish())
+}
